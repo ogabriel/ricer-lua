9234ce6c74eabd709284fcd5a5cc5201519d386e
ogabriel <gabrieloliver8991@gmail.com>
2023-12-20 09:00:14 -0300

    update

--- src/profile.lua
+++ src/profile.lua
@@ -1,11 +1,31 @@
 local setup = require("src.setup")
 
+local extra = {
+	packages = {
+		"mako",
+		"alacritty",
+		"waybar",
+		"swaylock",
+		"swayidle",
+		"rofi-lbonn-wayland",
+	},
+}
+
 return {
 	hyprland = {
 		setups = {
+			setup.pacman,
+			setup.yay,
+			setup.sddm,
+			setup.audio,
+			setup.bluetooth,
 			setup.network,
+			setup.screen,
+			setup.theme,
+			setup.fonts,
 			setup.thunar,
 			setup.hyprland,
+			extra,
 		},
 	},
 }
